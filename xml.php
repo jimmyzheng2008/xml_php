2bc5fa6e3f615be86f752c5c5df8045e476069e7
jimmy <jimmyzheng2008@yahoo.ca>
2015-02-28 00:44:11 -0500

    first php

---
+++ xml.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>xml example</title>
+</head>
+<body>
+  <div id="myId"></div>
+  <button type="button" name="myButton">Change content</button>
+</body>
+</html>
